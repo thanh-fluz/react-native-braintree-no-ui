d6af5b92fd9462e4fa1bc9cadb81d7d833017563
ken <kenneth@fluz.app>
2020-05-10 08:53:19 -0700

    feat(venmo): add vaulting for ios

--- ios/RCTBraintree/RCTBraintree.m
+++ ios/RCTBraintree/RCTBraintree.m
@@ -140,7 +140,7 @@ RCT_EXPORT_METHOD(showVenmoViewController:(RCTResponseSenderBlock)callback)
 
         BTVenmoDriver *venmoDriver = [[BTVenmoDriver alloc] initWithAPIClient:self.braintreeClient];
 
-        [venmoDriver authorizeAccountWithCompletion:^(BTVenmoAccountNonce * _Nullable venmoAccount, NSError * _Nullable error) {
+        [venmoDriver authorizeAccountAndVault:YES WithCompletion:^(BTVenmoAccountNonce * _Nullable venmoAccount, NSError * _Nullable error) {
             NSMutableArray *args = @[[NSNull null]];
             if ( error == nil && venmoAccount != nil ) {
                 if (venmoAccount.nonce != nil) {
